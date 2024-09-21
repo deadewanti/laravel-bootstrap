b1cdb8d074507d6273aa0f95ada42836b800aec2
dhea <dewantidea76@gmail.com>
2024-09-21 14:08:45 +0700

    first commit

---
+++ resources/views/componens/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="bg-light text-center text-lg-start mt-5" style="position: fixed; bottom: 0; left: 0; width: 100%;">
+<div class="text-center p-3" style="background-color: #787b7e; width: 100%; border-top: 1px solid #ddd;">
+    © {{ date('Y') }} Your Website:
+    <a class="text-dark" href="https://www.tiktok.com/cacanur541?_t=8ptkLrLjE9a&_r=1">Tiktok</a>
+</div>
+</footer>
